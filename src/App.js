e131c895390b8da55e78f69979c53c2a68383587
chenminjia <314081655@qq.com>
2019-07-27 10:09:32 +0800

    first react demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button/button.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Button value="按钮" />
+    </div>
+  );
+}
+
+export default App;
