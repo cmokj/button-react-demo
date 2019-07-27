907510c6573581a68a6b71a6141baeaf1f38a42d
chenminjia <314081655@qq.com>
2019-07-27 10:57:45 +0800

    重新build

---
+++ build/precache-manifest.ee72cf88014b8c9fa55601ee6881599b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4fce35575fca022ae8461f78d4450e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81c8c700548e720e716d",
+    "url": "./static/css/main.89ddf08b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "81c8c700548e720e716d",
+    "url": "./static/js/main.bc5390fa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
